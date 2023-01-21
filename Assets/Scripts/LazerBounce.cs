a7b40a8b54d8ad2b4cede717bedc8b553d44ee90
ShunPeng1 <shunpeng1@gmail.com>
2023-01-21 21:14:46 +0700

    Lazer

---
+++ Assets/Scripts/LazerBounce.cs
@@ -0,0 +1,90 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LazerBounce : MonoBehaviour
+{
+    private LineRenderer _laser;
+
+    private int _count ;
+    private GameObject _lastHitObject = null;
+    
+    [SerializeField] private Vector3 positionOffset;
+    [SerializeField] private float originalRotationDegree = 0;
+
+    [SerializeField] private int maxBounce = 20;
+    [SerializeField] private LayerMask collideLayerMask;
+
+    
+    
+    private void Start()
+    {
+        _laser = GetComponent<LineRenderer>();
+        _laser.positionCount = maxBounce;
+        
+    }
+    private void Update()
+    {
+        _count = 0;
+        CastLaser(transform.position + positionOffset, new Vector3( Mathf.Cos(originalRotationDegree*Mathf.Deg2Rad),Mathf.Sin(originalRotationDegree*Mathf.Deg2Rad) ,0));
+        
+    }
+    private void CastLaser(Vector3 position , Vector3 direction)
+    {
+        _laser.SetPosition(0, transform.position + positionOffset );
+        _lastHitObject = gameObject;
+        
+        for (int i=0; i< maxBounce; i++ )
+        {
+            
+            Ray ray = new Ray(position, direction);
+
+            if(_count < maxBounce - 1) _count++;
+
+            RaycastHit2D[] hits = Physics2D.RaycastAll(position, direction, 300, collideLayerMask );
+            
+            Array.Sort(hits,
+                delegate(RaycastHit2D x, RaycastHit2D y) { return x.distance.CompareTo(y.distance); });
+
+            
+            foreach (var hit in hits)
+            {
+                    
+                if ( hit.transform.gameObject == _lastHitObject)
+                {
+                    continue;
+                }
+
+                _lastHitObject = hit.transform.gameObject;
+                    
+                position = hit.point;
+                direction = Vector3.Reflect(direction, hit.normal);
+                _laser.SetPosition(_count, hit.point);
+                
+                if (hit.transform.CompareTag("Mirror")) 
+                {
+                    _laser.SetPosition(_count, hit.point);
+                }
+                else
+                {
+                    for (int j = (i + 1); j < maxBounce; j++)
+                    {
+                        _laser.SetPosition(j, hit.point);
+
+                    }
+                }
+                
+                goto SuccessfullyHit;
+                
+            }
+
+
+            _laser.SetPosition( _count, ray.GetPoint(300));
+
+            SuccessfullyHit: ;
+
+        }
+      
+    }
+}
